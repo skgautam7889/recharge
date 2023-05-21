03d8ecead2fb21f3a8ab55263abfc11e53c7674b
suraj <surajkumargautam7889@gmail.com>
2023-05-21 18:14:27 +0530

    some changes response

--- src/components/OrderSummary/index.jsx
+++ src/components/OrderSummary/index.jsx
@@ -146,33 +146,61 @@ const OrderSummary = (props) => {
                                     <h3 className="text-5 fw-400 mb-3 mb-sm-4 text-center">Confirm Recharge Details</h3>
                                     <hr className="mx-n3 mx-sm-n5 mb-4" />
                                     <div className="row">
-                                        <p className="col-sm text-muted mb-0 mb-sm-3">Mobile Number:</p>
-                                        <p className="col-sm text-sm-end fw-500"> {selectedPlan?.number}</p>
+                                        <div className="col-6 col-lg-6 text-3">
+                                            <p className="col-sm text-muted mb-0 mb-sm-3">Mobile Number:</p>
+                                        </div>
+                                        <div className="col-6 col-lg-6 text-3">
+                                            <p className="col-sm text-sm-end fw-500"> {selectedPlan?.number}</p>
+                                        </div>
                                     </div>
                                     <div className="row">
-                                        <p className="col-sm text-muted mb-0 mb-sm-3">Operator/Circle:</p>
-                                        <p className="col-sm text-sm-end fw-500">{selectedPlan?.biller_name} | {selectedPlan?.circle_name}</p>
+                                        <div className="col-6 col-lg-6 text-3">
+                                            <p className="col-sm text-muted mb-0 mb-sm-3">Operator/Circle:</p>
+                                        </div>
+                                        <div className="col-6 col-lg-6 text-3">
+                                            <p className="col-sm text-sm-end fw-500">{selectedPlan?.biller_name} | {selectedPlan?.circle_name}</p>
+                                        </div>
                                     </div>
                                     <div className="row">
-                                        <p className="col-sm text-muted mb-0 mb-sm-3">Plan:</p>
-                                        <p className="col-sm text-sm-end fw-500">{selectedPlan?.plan_category_name}</p>
+                                        <div className="col-6 col-lg-6 text-3">
+                                            <p className="col-sm text-muted mb-0 mb-sm-3">Plan:</p>
+                                        </div>
+                                        <div className="col-6 col-lg-6 text-3">
+                                            <p className="col-sm text-sm-end fw-500">{selectedPlan?.plan_category_name}</p>
+                                        </div>
                                     </div>
                                     <div className="row">
-                                        <p className="col-sm text-muted mb-0 mb-sm-3">Validity:</p>
-                                        <p className="col-sm text-sm-end fw-500">{selectedPlan?.validity}</p>
+                                        <div className="col-6 col-lg-6 text-3">
+                                            <p className="col-sm text-muted mb-0 mb-sm-3">Validity:</p>
+                                        </div>
+                                        <div className="col-6 col-lg-6 text-3">
+                                            <p className="col-sm text-sm-end fw-500">{selectedPlan?.validity}</p>
+                                        </div>
                                     </div>
                                     <div className="row">
-                                        <p className="col-sm text-muted mb-0 mb-sm-3">Amount:</p>
-                                        <p className="col-sm text-sm-end fw-500">${selectedPlan?.amount} </p>
+                                        <div className="col-6 col-lg-6 text-3">
+                                            <p className="col-sm text-muted mb-0 mb-sm-3">Amount:</p>
+                                        </div>
+                                        <div className="col-6 col-lg-6 text-3">
+                                            <p className="col-sm text-sm-end fw-500">${selectedPlan?.amount} </p>
+                                        </div>
                                     </div>
                                     <div className="row">
-                                        <p className="col-12 text-muted mb-0">Plan Description:</p>
-                                        <p className="col-12 text-1">{selectedPlan?.plan_description}</p>
+                                        <div className="col-6 col-lg-6 text-3">
+                                            <p className="col-12 text-muted mb-0">Plan Description:</p>
+                                        </div>
+                                        <div className="col-6 col-lg-6 text-3">
+                                            <p className="col-12 text-1" style={{ textAlign: 'justify' }}>{selectedPlan?.plan_description}</p>
+                                        </div>
                                     </div>
                                     <div className="bg-light-4 rounded p-3">
                                         <div className="row">
-                                            <div className="col-sm text-3 fw-600">Payment Amount:</div>
-                                            <div className="col-sm text-sm-end text-5 fw-500">${selectedPlan?.total_pay_amount}</div>
+                                            <div className="col-6 col-lg-6 text-3">
+                                                <div className="col-sm text-3 fw-600">Payment Amount:</div>
+                                            </div>
+                                            <div className="col-6 col-lg-6 text-3">
+                                                <div className="col-sm text-sm-end text-5 fw-500">${selectedPlan?.total_pay_amount}</div>
+                                            </div>
                                         </div>
                                     </div>
                                     <p className="text-center my-4"><a className="btn-link" data-bs-toggle="collapse" href="#couponCode" aria-expanded="false" aria-controls="couponCode">Apply a Coupon Code</a></p>
@@ -190,10 +218,9 @@ const OrderSummary = (props) => {
 
                                         }
                                         {coupons && coupons.map((coupon) => (
-                                            <div key={coupon.CouponCode}>
+                                            <div key={coupon.CouponCode} style={{textAlign:'justify'}}>
                                                 <span style={{ fontWeight: 'bold', lineHeight: 2.0 }} >{coupon.CouponCode}</span>
-                                                &nbsp;
-                                                <span>{coupon.Remarks}</span>
+                                                &nbsp; {coupon.Remarks}
                                                 <hr />
                                             </div>
                                         ))}
@@ -260,7 +287,7 @@ const OrderSummary = (props) => {
                                             <p className="col-sm text-muted mb-0 mb-sm-3">Remaining Amount:</p>
                                         </div>
                                         <div className="col-6 col-lg-6 text-3">
-                                            <p className="col-sm text-sm-end fw-500 text-right">{billplanInformation?.billlist[0]?.billamount-billplanInformation?.amount}</p>
+                                            <p className="col-sm text-sm-end fw-500 text-right">{billplanInformation?.billlist[0]?.billamount - billplanInformation?.amount}</p>
                                         </div>
                                     </div>
                                     <div className="bg-light-4 rounded p-3">
